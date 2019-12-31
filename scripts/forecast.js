bd718046628e77ad73fea50a9eb769776099a578
gustavocsverdrup <gustavo.catalasverdrup@gmail.com>
2019-12-31 01:47:56 +0100

    weather forecast function with details

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -4,7 +4,7 @@ searchEl=document.querySelector(".search");
 
 //String(searchEl.value);
 
-pictureEl = document.querySelector(".picture");
+//pictureEl = document.querySelector(".picture");
 
 iconEl = document.querySelector(".icon");
 
@@ -12,9 +12,10 @@ detailsEl = document.querySelector(".details");
 
 cityEl = document.querySelector(".city");
 
-forecastEL = document.querySelector(".forecast");
+forecastEl = document.querySelector(".forecast");
 
 tempEl = document.querySelector(".temp");
+
 
 // EVent listener on the search
 
@@ -26,14 +27,40 @@ searchInput = String(searchEl.value);
 
 getCityDetails (searchInput);
 
+
+
 });
 
+//Get city ID
 
 const getCityDetails = (searchInput)=>{
+
+    cityEl.innerText=searchInput;
 
         fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=WeERAuEOIhhjZODtsMFPY5snLAJOk3Jz&q=${searchInput}`)    
         
         .then(response =>response.json())
-        .then(response=>console.log(response[0].Key))
+        .then(response=>getForecast(response[0].Key))
         .catch(err=>console.log(err))};
 
+//Search weather details
+
+const getForecast = (city) => {
+
+   // searchInput="";
+   // searchEl.value="";
+
+    fetch(`http://dataservice.accuweather.com/currentconditions/v1/${city}?apikey=WeERAuEOIhhjZODtsMFPY5snLAJOk3Jz`)    
+        
+        .then(response =>response.json())
+        .then(response=>outputWeather(response[0]))
+        .catch(err=>console.log(err))};
+
+
+const outputWeather = (forecast) => {
+
+    forecastEl.innerHTML = forecast.WeatherText;
+    tempEl.innerText= forecast.Temperature.Metric.Value;
+    iconEl.src=forecast.WeatherIcon;
+
+}
