20160f2b14ff0c1b764a7ae410d54119239ecb2a
gustavocsverdrup <gustavo.catalasverdrup@gmail.com>
2020-01-02 19:06:30 +0100

    With localStorage and picture of night / day

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -4,7 +4,7 @@ searchEl=document.querySelector(".search");
 
 //String(searchEl.value);
 
-//pictureEl = document.querySelector(".picture");
+pictureEl = document.querySelector(".picture");
 
 iconEl = document.querySelector(".icono");
 
@@ -36,6 +36,7 @@ getCityDetails (searchInput);
 const getCityDetails = (searchInput)=>{
 
     cityEl.innerText=searchInput;
+    updateStorage(searchInput);
 
         fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=WeERAuEOIhhjZODtsMFPY5snLAJOk3Jz&q=${searchInput}`)    
         
@@ -62,16 +63,40 @@ const outputWeather = (forecast) => {
     forecastEl.innerHTML = forecast.WeatherText;
     tempEl.innerText= forecast.Temperature.Metric.Value;
 
-    if (forecast.WeatherText="Mostly clear") {
+    if (forecast.IsDayTime) {pictureEl.src="images/cloudy.png";}
+
+    else {pictureEl.src="images/night.png"};
+
+    if (forecast.WeatherText=="Mostly clear") {
     iconEl.src=`images/partly-cloudy.png`;
     }
 
-    else if (forecast.WeatherText="Sunny") {
+    else if (forecast.WeatherText=="Sunny") {
         iconEl.src=`images/sunny.png`;
         }
     
-    else if (forecast.WeatherText="Rainy") {
+    else if (forecast.WeatherText=="Rainy") {
         iconEl.src=`images/rainy.png`;
         }
+    else if (forecast.WeatherText=="Cloudy") {
+            iconEl.src=`images/cloudy.png`;
+            }
+    
+}
+
+const updateStorage = (city) => {
+
+    localStorage.clear();
+
+    localStorage.setItem('city', city);
+}
+
+const getFromStorage = () => {
+
+    const city = localStorage.getItem('city');
+
+    getCityDetails(city);
 
 }
+
+getFromStorage();
